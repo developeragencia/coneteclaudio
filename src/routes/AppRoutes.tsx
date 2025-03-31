bc41321f50feff86c36f7808a2003ad21afaf815
developeragencia <agenciadeveloper01@gmail.com>
2025-03-31 20:23:28 -0300

    Updateapptoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,27 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { adminRoutes } from './AdminRoutes';
-import { AuthLayout } from '@/layouts/AuthLayout';
-import { AdminLayout } from '@/layouts/AdminLayout';
 import { LoginPage } from '@/pages/Login';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
       
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<LoginPage />} />
-      </Route>
-
-      <Route element={<AdminLayout />}>
-        {adminRoutes.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={route.element}
-          />
-        ))}
-      </Route>
+      {adminRoutes.map((route) => (
+        <Route
+          key={route.path}
+          path={route.path}
+          element={route.element}
+        />
+      ))}
     </Routes>
   );
 } 
